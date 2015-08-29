1f189766adb5854bb2e5202c70f2816787aca204
Ben Menking <ben@menking.net>
2015-08-29 14:29:38 -0400

    More examples added
    by Mpapec on Dec 23rd, 2010

---
+++ examples/example3.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Example for adding a VPN user */
+
+require('../routeros_api.class.php');
+
+$API = new routeros_api();
+
+$API->debug = true;
+
+if ($API->connect('111.111.111.111', 'LOGIN', 'PASSWORD')) {
+
+   $API->comm("/ppp/secret/add", array(
+      "name"     => "user",
+      "password" => "pass",
+      "remote-address" => "172.16.1.10",
+      "comment"  => "{new VPN user}",
+      "service"  => "pptp",
+   ));
+
+   $API->disconnect();
+
+}
+
+?>
---
+++ examples/example4.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Example of finding registration-table ID for specified MAC */
+
+require('../routeros_api.class.php');
+
+$API = new routeros_api();
+
+$API->debug = true;
+
+if ($API->connect('111.111.111.111', 'LOGIN', 'PASSWORD')) {
+
+   $ARRAY = $API->comm("/interface/wireless/registration-table/print", array(
+      ".proplist"=> ".id",
+      "?mac-address" => "00:0E:BB:DD:FF:FF",
+   ));
+	
+   print_r($ARRAY);
+
+   $API->disconnect();
+
+}
+
+?>
