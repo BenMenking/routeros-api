c6db261bb6715426983716aa6dae6792c28545e7
exmac <ex.mac.work@gmail.com>
2015-12-21 16:45:05 +0100

    read unparsed result, and parse it

--- examples/example2.php
+++ examples/example2.php
@@ -11,7 +11,7 @@ if ($API->connect('111.111.111.111', 'LOGIN', 'PASSWORD')) {
    $API->write('/interface/wireless/registration-table/print',false);
    $API->write('=stats=');
  
-   $READ = $API->read();
+   $READ = $API->read(false);
    $ARRAY = $API->parseResponse($READ);
 
    print_r($ARRAY);
