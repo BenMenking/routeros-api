54f092d7b5eb0c10a516fae508bed3940b17eb07
Eduardo <eduardomazolini@users.noreply.github.com>
2019-03-06 12:49:14 -0300

    Update routeros_api.class.php

--- routeros_api.class.php
+++ routeros_api.class.php
@@ -97,7 +97,10 @@ class RouterosAPI
         for ($ATTEMPT = 1; $ATTEMPT <= $this->attempts; $ATTEMPT++) {
             $this->connected = false;
             $PROTOCOL = ($this->ssl ? 'ssl://' : '' );
-            $context = stream_context_create(array('ssl' => array('ciphers' => 'ADH:ALL', 'verify_peer' => false, 'verify_peer_name' => false)));
+            /**
+            * IF USE OPENSSL OLD 1.1.0 remove :@SECLEVEL=0
+            **/
+            $context = stream_context_create(array('ssl' => array('ciphers' => 'ADH:ALL:@SECLEVEL=0', 'verify_peer' => false, 'verify_peer_name' => false)));
             $this->debug('Connection attempt #' . $ATTEMPT . ' to ' . $PROTOCOL . $ip . ':' . $this->port . '...');
             $this->socket = @stream_socket_client($PROTOCOL . $ip.':'. $this->port, $this->error_no, $this->error_str, $this->timeout, STREAM_CLIENT_CONNECT,$context);
             if ($this->socket) {
